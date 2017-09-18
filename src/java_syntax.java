b6a6546bcc121b74fbb334c59b81ed97353121ee
Elvin <ttop34@icloud.com>
2017-09-18 15:33:55 -0500

    solutuion for first exercise

---
+++ src/java_syntax.java
@@ -0,0 +1,20 @@
+public class java_syntax {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 6;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "string";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+//
+//        int x = 4;
+//        System.out.println(x += 5);
+
+        int x = 10;
+        int y = 2;
+        System.out.println( y - x);
+
+    }
+}
