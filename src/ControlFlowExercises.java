97675e28788dbac0fd352ac73bdfb46afd63ef15
Elvin <ttop34@icloud.com>
2017-09-19 14:41:17 -0500

    complete fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,23 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
+        String y = "fizz";
+        String x = "buzz";
 
-        for (int i = 100; i >= -10; i-=5) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+            } else {
+                if (i % 3 == 0) {
+                    System.out.print(y);
+                }
 
+                if (i % 5 == 0) {
+                    System.out.print(x);
+                }
+                System.out.println();
+            }
         }
 
-        for (long j = 2; j < 100000; j *= j) {
-            System.out.println(j);
-        }
     }
 }
