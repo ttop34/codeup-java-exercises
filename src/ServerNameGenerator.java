0cf17e346d9783764bc6449721ae60eea0c16978
Elvin <ttop34@icloud.com>
2017-09-26 12:23:37 -0500

    completed server name generator exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+public class ServerNameGenerator {
+
+    public static void main(String[] args) {
+    String[] adjectives = new String[]{"Fuzzy", "Relaxed", "Solid", "Angular", "Frosted", "Lucid", "Antiquated", "Hollow", "Binary", "Pixelated"};
+
+        String[] nouns = new String[]{"Nebula", "Parallax", "Quandary", "Nova", "Singularity", "Dwarf", "Graviton", "Equinox", "Light Year", "Continuum"};
+
+        System.out.println("Here is your server name:");
+        System.out.print(getString(adjectives));
+        System.out.print("-");
+        System.out.print(getString(nouns));
+
+
+    }
+
+
+
+    public static String getString(String[] array) {
+
+        int random = (int) (Math.random()* array.length);
+        return array[random];
+
+    }
+
+
+
+
+
+
+
+}
+
--- src/Util/Shapes/CircleApp.java
+++ src/Util/Shapes/CircleApp.java
@@ -5,8 +5,8 @@ public class CircleApp {
     public static void main(String[] args) {
         Input prompt = new Input();
         double userInput = prompt.getDouble();
-
         Circle circle = new Circle(userInput);
+
         System.out.println(circle.getCircumference());
         System.out.println(circle.getArea());
 
