e473676c2d6638806362f86da402fd51b2f8e0e2
Elvin <ttop34@icloud.com>
2017-09-29 12:30:22 -0500

    completed ex

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,7 +4,7 @@ public class ShapesTest {
     public static void main(String[] args) {
         Measurable myShape;
 
-        myShape = new Rectangle(5,4;
+        myShape = new Rectangle(5,4);
 //        myShape = new Square(5);
 
 
